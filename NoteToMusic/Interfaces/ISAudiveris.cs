bb9786be57105f8023c295ccbb511af4173f6b93
yigitemrertn <yigitemreerten@gmail.com>
2025-12-25 11:02:37 +0300

    Gelişmiş Media Player ve PDF desteği(beta)

--- NoteToMusic/Interfaces/ISAudiveris.cs
+++ NoteToMusic/Interfaces/ISAudiveris.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace NoteToMusic.Interfaces
@@ -13,5 +15,13 @@ namespace NoteToMusic.Interfaces
         /// <param name="imagePath">Görüntü dosyasının yolu</param>
         /// <returns>Oluşturulan XML dosyasının yolu veya hata mesajı</returns>
         Task<string> ConvertImageToMusicXmlAsync(string imagePath);
+
+        /// <summary>
+        /// Birden fazla görüntü dosyasını sırayla MusicXML formatına dönüştürür
+        /// </summary>
+        /// <param name="imagePaths">Görüntü dosyalarının yol listesi</param>
+        /// <param name="progress">İlerleme raporlama için (opsiyonel)</param>
+        /// <returns>Oluşturulan XML dosyalarının yol listesi</returns>
+        Task<List<string>> ConvertMultipleImagesToMusicXmlAsync(List<string> imagePaths, IProgress<int> progress = null);
     }
 }
